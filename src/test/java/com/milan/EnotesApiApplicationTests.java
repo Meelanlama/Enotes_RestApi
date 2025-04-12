eb1bb75f84ed552f72becc0f173841518e8a8164
Milan <lamameelan32@gmail.com>
2025-04-12 18:40:40 +0545

    Different profiles. changed to yml from properties

--- src/test/java/com/milan/EnotesApiApplicationTests.java
+++ src/test/java/com/milan/EnotesApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.milan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class EnotesApiApplicationTests {
 
 	@Test
